46d4d7054a6b0eb2ad99d2d0a3b67e7966e6ea81
Lisa Nguyen <ngu0087@gmail.com>
2024-05-31 16:49:09 +0800

    feat(index): create homepage with introductory content

---
+++ tailwind.config.js
@@ -0,0 +1,18 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  content: [
+      './*.html',
+  ],
+  theme: {
+    extend: {
+      colors: {
+        cruise100: '#d4f3e9',
+        cruise400: '#4ab79d',
+        cruise500: '#319b85',
+        cruise700: '#216458',
+      },
+    },
+  },
+  plugins: [],
+}
+
